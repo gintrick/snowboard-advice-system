cd1974b3072bfeeb746fb10bb7edba0d9ab0dd71
atsuo1203 <dondonkitemasu@gmail.com>
2023-03-08 16:17:16 +0900

    qr

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className='root'>
-      <div className='title'>レッスン不要でスノボが上達！！！</div>
+      <div className='title'>レッスン不要でスノボが上達！</div>
       <div className='imgs'>
         <img className='img' src='/cap_1.PNG' alt='cap1' />
         <img className='img' src='/cap_2.PNG' alt='cap2' />
+      </div>
+      <div className='title'>まずは公式LINEに登録！</div>
+      <div className='imgs'>
+        <img className='img' src='/qr.png' alt='qr' />
       </div>
     </div>
   );
