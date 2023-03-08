331cc0357dccd5120285a820d432c5bc37f81d24
atsuo1203 <dondonkitemasu@gmail.com>
2023-03-08 16:56:34 +0900

    お問い合わせ作成

--- src/App.js
+++ src/App.js
@@ -2,25 +2,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className='root'>
-      <div className='title'>レッスン不要でスノボが上達！</div>
-      <div className='imgs'>
-        <img className='img' src='/cap_1.PNG' alt='cap1' />
-        <img className='img' src='/cap_2.PNG' alt='cap2' />
+    <>
+      <div className='root'>
+        <div className='title'>レッスン不要でスノボが上達！</div>
+        <div className='imgs'>
+          <img className='img' src='/cap_1.PNG' alt='cap1' />
+          <img className='img' src='/cap_2.PNG' alt='cap2' />
+        </div>
+        <div className='title'>料金・サービス</div>
+        <div className='description'>
+          <li>
+            お客様が映っているのスノーボードの追いどり動画を公式LINEに送っていただきます。
+          </li>
+          <li>一回投稿につき、300円(税込)かかります</li>
+          <li>
+            投稿された動画に対して、インストラクターがアドバイスを返します
+          </li>
+        </div>
+        <div className='title'>登録はコチラから</div>
+        <div className='imgs'>
+          <img className='img' src='/qr.png' alt='qr' />
+        </div>
       </div>
-      <div className='title'>料金・サービス</div>
-      <div className='description'>
-        <li>
-          お客様が映っているのスノーボードの追いどり動画を公式LINEに送っていただきます。
-        </li>
-        <li>一回投稿につき、300円(税込)かかります</li>
-        <li>投稿された動画に対して、インストラクターがアドバイスを返します</li>
+      <div className='footer'>
+        <div className='footerContent'>お問い合わせ: gin@gintrick.com</div>
+        <div className='footerContent'>会社概要: https://gintrick.com</div>
       </div>
-      <div className='title'>登録はコチラから</div>
-      <div className='imgs'>
-        <img className='img' src='/qr.png' alt='qr' />
-      </div>
-    </div>
+    </>
   );
 }
 
