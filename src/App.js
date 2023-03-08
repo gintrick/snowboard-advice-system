af3e421a8d83bddf78c88b8cf51c63dd81fc1687
atsuo1203 <dondonkitemasu@gmail.com>
2023-03-08 16:11:13 +0900

    lineのキャプチャを貼り付けた

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='root'>
+      <div className='title'>レッスン不要でスノボが上達！！！</div>
+      <div className='imgs'>
+        <img className='img' src='/cap_1.PNG' alt='cap1' />
+        <img className='img' src='/cap_2.PNG' alt='cap2' />
+      </div>
     </div>
   );
 }
