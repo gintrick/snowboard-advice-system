d9006b091e3baf65b39e371bbb2c7d212837198f
atsuo1203 <dondonkitemasu@gmail.com>
2023-03-08 16:24:51 +0900

    料金システム

--- src/App.js
+++ src/App.js
@@ -8,7 +8,15 @@ function App() {
         <img className='img' src='/cap_1.PNG' alt='cap1' />
         <img className='img' src='/cap_2.PNG' alt='cap2' />
       </div>
-      <div className='title'>まずは公式LINEに登録！</div>
+      <div className='title'>料金・サービス</div>
+      <div>
+        <li>
+          お客様が映っているのスノーボードの追いどり動画を公式LINEに送っていただきます。
+        </li>
+        <li>一回投稿につき、300円(税込)かかります</li>
+        <li>投稿された動画に対して、インストラクターがアドバイスを返します</li>
+      </div>
+      <div className='title'>登録はコチラから</div>
       <div className='imgs'>
         <img className='img' src='/qr.png' alt='qr' />
       </div>
